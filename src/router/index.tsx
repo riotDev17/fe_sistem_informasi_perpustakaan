59f47bf5792d13e4672f786653caa65ba7778c65
riotDev17 <tomasalberto527@gmail.com>
2024-01-03 22:43:11 +0700

    protected route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,11 +2,19 @@ import { createBrowserRouter } from 'react-router-dom';
 import BlankLayout from '../layouts/BlankLayout';
 import DefaultLayout from '../layouts/DefaultLayout';
 import { routes } from './routes';
+import ProtectedRoute from '../configs/ProtectedRoute';
 
 const finalRoutes = routes.map((route) => {
   return {
     ...route,
-    element: route.layout === 'blank' ? <BlankLayout>{route.element}</BlankLayout> : <DefaultLayout>{route.element}</DefaultLayout>,
+    element:
+      route.layout === 'blank' ? (
+        <BlankLayout>{route.element}</BlankLayout>
+      ) : (
+        <ProtectedRoute>
+          <DefaultLayout>{route.element}</DefaultLayout>
+        </ProtectedRoute>
+      ),
   };
 });
 

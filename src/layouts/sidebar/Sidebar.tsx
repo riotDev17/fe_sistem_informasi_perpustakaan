844bc1ae94c858cf6717f37c042320092fb3fbff
riotDev17 <tomasalberto527@gmail.com>
2024-01-06 10:43:40 +0700

    chore: change name APP

--- src/layouts/sidebar/Sidebar.tsx
+++ src/layouts/sidebar/Sidebar.tsx
@@ -54,8 +54,8 @@ const Sidebar = () => {
         <div className="bg-white dark:bg-black h-full">
           <div className="flex justify-between items-center px-4 py-3">
             <NavLink to="/" className="main-logo flex items-center shrink-0">
-              {/* <img className="w-8 ml-[5px] flex-none" src="/assets/images/logo.svg" alt="logo" /> */}
-              <span className="text-2xl ltr:ml-1.5 rtl:mr-1.5 font-semibold align-middle lg:inline dark:text-white-light">{t('Book Verse Hub')}</span>
+              <img className="w-8 ml-[5px] flex-none" src="/assets/images/logo2.png" alt="logo" />
+              <span className="text-2xl ltr:ml-3 rtl:mr-1.5 font-semibold align-middle lg:inline dark:text-white-light">{t('IntelliBook')}</span>
             </NavLink>
 
             <button

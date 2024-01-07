64c9a52e797c193394d5114c18e921b2682b0dde
riotDev17 <tomasalberto527@gmail.com>
2024-01-07 21:11:14 +0700

    breadcrumbs

--- src/components/breadcrumbs/BreadcrumbsDefault.tsx
+++ src/components/breadcrumbs/BreadcrumbsDefault.tsx
@@ -15,13 +15,13 @@ const BreadcrumbsDefault: React.FC<BreadcrumbsDefaultProps> = ({ menus }) => {
     <>
       <ol className="flex text-gray-500 font-semibold dark:text-white-dark gap-4">
         {menus.map((menu, index) => (
-          <li key={index} className={index > 0 ? "before:content-['/'] before:mr-4 text-gray-800 text-base" : 'text-primary'}>
+          <li key={index} className={index > 0 ? "before:content-['/'] before:mr-4 text-gray-800 text-sm" : 'text-primary'}>
             {menu.link ? (
               <Link to={menu.link}>
                 <button
                   className={
                     menu.icon
-                      ? 'hover:text-gray-800/70 dark:hover:text-white-dark/70 text-base'
+                      ? 'hover:text-gray-800/70 dark:hover:text-white-dark/70 text-sm'
                       : `${index === 0 ? 'text-blue-500' : 'text-gray-800'} ${index === 0 ? 'dark:text-blue-200' : 'dark:text-gray-400'} hover:text-black/70 dark:hover:text-white-light/70`
                   }
                 >
